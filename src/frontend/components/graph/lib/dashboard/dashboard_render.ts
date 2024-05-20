f8d3f26543bddd7278ff96e7be0ca5599468d752
Dave Roberts <d.roberts@ctrlo.com>
2024-05-20 15:41:56 +0100

    Added dashboard renderer

--- src/frontend/components/graph/lib/dashboard/dashboard_render.ts
+++ src/frontend/components/graph/lib/dashboard/dashboard_render.ts
@@ -1,6 +1,6 @@
 // Why is this here rather than in the dashboard file?
+import { ChartRenderer } from "util/charts";
 import do_plot from "../chart";
-import { Chart } from "chart.js";
 
 /**
  * Render the dashboard graph from BASE64 encoded JSON strings - this is for the dashboard
@@ -9,7 +9,7 @@ import { Chart } from "chart.js";
  * @param {HTMLCanvasElement} canvas Canvas element to render the plot
  * @returns {Chart} The Chart component
  */
-export default async function do_plot_json (plotData: string, options_in: string, canvas: HTMLCanvasElement):Promise<Chart> {
+export default async function do_plot_json (plotData: string, options_in: string, canvas: HTMLCanvasElement):Promise<ChartRenderer> {
     const d = JSON.parse(atob(plotData))
     d.type === "donut" && (d.type = "doughnut");
     const o = JSON.parse(atob(options_in))

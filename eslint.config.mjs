9ac9a286b363efcc0b07aab91add9b4d3ec65875
Dave Roberts <d.roberts@ctrlo.com>
2024-10-04 14:39:21 +0100

    Updated ESLint
    Updated version of ESLint and added config - JS files have _not_ had linting performed

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default [
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  },
+  {
+    ignores: [
+      "public",
+      "src/frontend/components/dashboard/lib/react/polyfills",
+      "babel.config.js",
+      "webpack.config.js",
+      "jest.config.js",
+      "tsconfig.json",
+      "src/frontend/js/lib/jqplot",
+      "src/frontend/js/lib/jquery",
+      "src/frontend/js/lib/plotly",
+      "src/frontend/components/timeline",
+      "fengari-web.js",
+      "cypress.config.ts",
+      "src/frontend/testing/**/*",
+      "eslint.config.mjs"
+    ]
+  },
+  { files: ["./src/frontend/**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.jest, ...globals.jquery } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  stylistic.configs["recommended-flat"],
+  {plugins:
+    {"@stylistic": stylistic},
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-expressions": 0,
+      "@stylistic/brace-style": 0,
+      "@stylistic/semi": [2, "always"]
+    }
+  }
+];

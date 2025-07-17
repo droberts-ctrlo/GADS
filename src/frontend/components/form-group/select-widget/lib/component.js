1238a770968ee85e22223b004a7584755ddc4507
Dave Roberts <d.roberts@ctrlo.com>
2025-07-17 12:09:37 +0100

    Formatted and changed code with ESLint fixes - functionality has not changed
    Removed cjs from eslint - cjs are used for internal development files, and should never be included

--- src/frontend/components/form-group/select-widget/lib/component.js
+++ src/frontend/components/form-group/select-widget/lib/component.js
@@ -286,14 +286,8 @@ class SelectWidgetComponent extends Component {
 
     /**
      * Connects the multi-select items to their associated checkboxes.
-<<<<<<< HEAD
-<<<<<<< HEAD
      * @returns {(...*)=>*} A function that connects the multi-select items.
      * @todo Remove deprecated key codes and ensure compatibility with modern browsers.
-=======
->>>>>>> bb34593f4 (Updated with changes within current dev)
-=======
->>>>>>> 3cacb9c64 (Updated code files with changes as required where they were included in dev)
      */
     connectMulti() {
         const self = this;
@@ -641,16 +635,9 @@ class SelectWidgetComponent extends Component {
     }
 
     /**
-<<<<<<< HEAD
-<<<<<<< HEAD
      * Fetch options for the select widget based on linked fields.
      * @throws {string} Will throw an error if the filter fields are not a valid array.
-=======
      * Fetches options for the select widget based on linked fields.
->>>>>>> bb34593f4 (Updated with changes within current dev)
-=======
-     * Fetches options for the select widget based on linked fields.
->>>>>>> 3cacb9c64 (Updated code files with changes as required where they were included in dev)
      */
     fetchOptions() {
         const filterEndpoint = this.$selectWidget.data('filter-endpoint');

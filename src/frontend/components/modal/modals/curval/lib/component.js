8fd88ff307e8fc0a2a6472c5fe6ccdd074ed1bc4
Dave Roberts <d.roberts@ctrlo.com>
2025-08-04 10:04:35 +0100

    Fixed formatting and code where this was broken on merge with current dev branch
    Updated and removed extra dependencies and exports where not required
    
    Further package changes and TSConfig updates
    
    Removed knip
    
    Fixed wrong TSC version

--- src/frontend/components/modal/modals/curval/lib/component.js
+++ src/frontend/components/modal/modals/curval/lib/component.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-this-alias */
 import ModalComponent from '../../../lib/component';
 import { setFieldValues } from 'set-field-values';
-import { guid as Guid } from 'guid';
 import { initializeRegisteredComponents } from 'component';
 import { validateRadioGroup, validateCheckboxGroup } from 'validation';
 import { fromJson } from 'util/common';
@@ -152,7 +151,7 @@ class CurvalModalComponent extends ModalComponent {
             // guids in the autosave
             let is_new_row;
             if (!guid && !current_id) {
-                guid = Guid();
+                guid = crypto.randomUUID();
                 is_new_row = true;
             }
             const hidden_input = $('<input>').attr({
@@ -193,7 +192,7 @@ class CurvalModalComponent extends ModalComponent {
                 $answersList.find('li input').prop('checked', false);
             }
 
-            guid ||= Guid();
+            guid ||= crypto.randomUUID();
             const id = `field${col_id}_${guid}`;
             const deleteButton = multi
                 ? '<button class="close select-widget-value__delete" aria-hidden="true" aria-label="delete" title="delete" tabindex="-1">&times</button>'
@@ -351,7 +350,7 @@ class CurvalModalComponent extends ModalComponent {
             if (mode === 'edit') {
                 guid = hidden.data('guid');
                 if (!guid) {
-                    guid = Guid();
+                    guid = crypto.randomUUID();
                     hidden.attr('data-guid', guid);
                 }
             }
